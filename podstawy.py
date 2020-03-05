c0510d1fe85d8bf48d5e1d0cda145b6072068463
Joanna Kaźmierczak <jkazmierczak@edu.cdv.pl>
2020-03-05 17:20:42 +0100

    first lesson

--- podstawy.py
+++ podstawy.py
@@ -1,1 +1,48 @@
 print("janusz")
+print(2)
+'''
+komantarz blokowy
+'''
+#potegowanie
+potega = 2 ** 10
+print(potega)
+
+#pobieranie z klawiattury
+name=input()
+'''
+print("twoje imie podane z klawiatury: " + name)
+
+surname=input()
+print("imie: " + name + ", nazwisko: " + surname)
+print("podaj swoj wiek: ", end="")
+age=input()
+
+age1=20
+print(type(age1))
+print("twoj wiek: ",age1," lata")
+
+############################################
+
+
+surname="kowalski"
+firstChar = surname[0]
+print(firstChar)
+
+lenght=len(surname)
+print(lenght)
+
+lastChar=surname[len(surname)-1]
+print(lastChar)
+'''
+#konwersja
+x="5"
+print(type(x))
+x=int(x)
+
+y=4
+print(type(y))
+y=y/2
+print(type(y))
+print(y)
+
+print(type(x))
